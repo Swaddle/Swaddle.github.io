0b4c6895afd61f9bcbcace117760186e723de4c3
Michael <meswaddle@netspace.net.au>
2014-07-15 17:12:58 +0800

    colour

--- hexagon.js
+++ hexagon.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var coords = [[520, 360], [620, 310], [720, 360], [820, 310], [920, 360], [620, 410], [820, 410]];
+var coords = [[520, 360], [625, 310], [720, 360], [820, 310], [920, 360], [620, 410], [815, 410]];
 var numberOfSides = 6;
 var size = 50;
 var i;
@@ -18,11 +18,13 @@ for (pt = 0; pt < coords.length; pt += 1) {
 
     for (i = 1; i <= numberOfSides; i += 1) {
         ctx.lineTo(x + size * Math.cos(i * 2 * Math.PI / numberOfSides), y + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+        ctx.lineJoin;
     }
 
-    ctx.strokeStyle = "#000000";
-    ctx.fillStyle = (Math.random().toString(16) + '000000').slice(2, 8);
-    ctx.lineWidth = 2;
+    ctx.strokeStyle = (Math.random().toString(16) + '000000').slice(2, 8);
+    ctx.lineWidth = 3;
     ctx.stroke();
+    ctx.fillStyle = "#fff";
+    ctx.fill();
 }
 
